0e11f683686a32a33d6f823da9570ae8830ae4b4
Joe <JA@Boost.local>
2013-06-12 20:04:56 +1200

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vlad::Application.config.secret_token = 'ff4effe4bec37a4d52db8c7d486bb9a6e432c3a438dd559de0921060653454e3b986ea94c2cd0881e2cc29d73448f3e4c0c19ebd9f2b679eee46bc547d71ee32'
